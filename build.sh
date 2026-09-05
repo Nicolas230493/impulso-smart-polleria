537fd036ca907c54f1d9c1c9de60d9ed2424b0b3
Nico <theperfectmelody@hotmail.com.ar>
2026-09-05 19:55:13 -0300

    Auto create superuser on build

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin');
+    print('Superusuario admin creado con éxito.');
+else:
+    print('El superusuario admin ya existe.');
+"
